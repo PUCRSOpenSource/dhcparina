80174c0c43a82533017947e2acce93681ae73d9a
Matthias Nunes <matthiasnunes@gmail.com>
2018-06-30 23:20:09 -0300

    Fills dhcp header

--- src/main.c
+++ src/main.c
@@ -37,11 +37,13 @@ char* ip_str;
 struct ether_header* r_eh;
 struct iphdr* r_iphdr;
 struct udphdr* r_udp_header;
+struct dhcp_packet* r_dhcp_header;
 
 // Headers for accessing the write_buffer
 struct ether_header* w_eh;
 struct iphdr* w_iphdr;
 struct udphdr* w_udp_header;
+struct dhcp_packet* w_dhcp_header;
 
 void
 setup(char* argv[])
@@ -79,11 +81,14 @@ setup(char* argv[])
 	r_eh = (struct ether_header*) read_buffer;
 	r_iphdr = (struct iphdr*) (read_buffer + sizeof(struct ether_header));
 	r_udp_header = (struct udphdr*) (read_buffer + (sizeof(struct ether_header) + sizeof(struct iphdr)));
+	r_dhcp_header = (struct dhcp_packet*) (read_buffer + (sizeof(struct ether_header) + sizeof(struct iphdr) + sizeof(struct udphdr)));
+
 
 	//Setup access pointers for write_buffer
 	w_eh = (struct ether_header*) write_buffer;
 	w_iphdr = (struct iphdr*) (write_buffer + sizeof(struct ether_header));
 	w_udp_header = (struct udphdr*) (write_buffer + (sizeof(struct ether_header) + sizeof(struct iphdr)));
+	w_dhcp_header = (struct dhcp_packet*) (write_buffer + (sizeof(struct ether_header) + sizeof(struct iphdr) + sizeof(struct udphdr)));
 }
 
 int
@@ -141,6 +146,76 @@ send_dhcp_discover(char* dst_addr)
 	w_udp_header->dest = htons(68);
 	w_udp_header->len = htons(0x13c);
 	w_udp_header->check = htons(0);
+
+	//Fill dhcp header
+	w_dhcp_header->op = 2;
+	w_dhcp_header->htype = 1;
+	w_dhcp_header->hlen = 6;
+	w_dhcp_header->hops = 0;
+	w_dhcp_header->xid = r_dhcp_header->xid;
+	w_dhcp_header->secs = 0;
+	w_dhcp_header->flags = 0;
+	w_dhcp_header->ciaddr = r_dhcp_header->ciaddr;
+	inet_aton(dst_addr, &w_dhcp_header->yiaddr);
+	w_dhcp_header->siaddr = r_dhcp_header->siaddr;
+	w_dhcp_header->giaddr = r_dhcp_header->giaddr;
+	for (int i = 0; i < 6; i++)
+	{
+		w_dhcp_header->chaddr[i] = r_eh->ether_shost[i];
+	}
+	//Fill magic cookie
+	w_dhcp_header->options[0] = 0x63;
+	w_dhcp_header->options[1] = 0x82;
+	w_dhcp_header->options[2] = 0x53;
+	w_dhcp_header->options[3] = 0x63;
+	//Fill message type
+	w_dhcp_header->options[4] = 53;
+	w_dhcp_header->options[5] = 1;
+	w_dhcp_header->options[6] = 2; // will need to be parametrized later
+	//Fill server identifier
+	w_dhcp_header->options[7] = 54;
+	w_dhcp_header->options[8] = 4;
+	w_dhcp_header->options[9] = (ip_int >> 24) & 255;
+	w_dhcp_header->options[10] = (ip_int >> 16) & 255;
+	w_dhcp_header->options[11] = (ip_int >> 8) & 255;
+	w_dhcp_header->options[12] = ip_int & 255;
+	//Fill dhcp subnet mask
+	w_dhcp_header->options[13] = 1;
+	w_dhcp_header->options[14] = 4;
+	w_dhcp_header->options[15] = 255;
+	w_dhcp_header->options[16] = 255;
+	w_dhcp_header->options[17] = 255;
+	w_dhcp_header->options[18] = 0;
+	//Fill dhcp address lease time
+	w_dhcp_header->options[19] = 51;
+	w_dhcp_header->options[20] = 4;
+	w_dhcp_header->options[21] = 0;
+	w_dhcp_header->options[22] = 1;
+	w_dhcp_header->options[23] = 56;
+	w_dhcp_header->options[24] = 128;
+	//Fill dhcp router
+	w_dhcp_header->options[25] = 3;
+	w_dhcp_header->options[26] = 4;
+	w_dhcp_header->options[27] = (ip_int >> 24) & 255;
+	w_dhcp_header->options[28] = (ip_int >> 16) & 255;
+	w_dhcp_header->options[29] = (ip_int >> 8) & 255;
+	w_dhcp_header->options[30] = ip_int & 255;
+	//Fill dhcp dns
+	w_dhcp_header->options[31] = 6;
+	w_dhcp_header->options[32] = 4;
+	w_dhcp_header->options[33] = (ip_int >> 24) & 255;
+	w_dhcp_header->options[34] = (ip_int >> 16) & 255;
+	w_dhcp_header->options[35] = (ip_int >> 8) & 255;
+	w_dhcp_header->options[36] = ip_int & 255;
+	//Fill dhcp broadcast
+	w_dhcp_header->options[37] = 28;
+	w_dhcp_header->options[38] = 4;
+	w_dhcp_header->options[39] = 255;
+	w_dhcp_header->options[40] = 255;
+	w_dhcp_header->options[41] = 255;
+	w_dhcp_header->options[42] = 255;
+	//Fill end =D
+	w_dhcp_header->options[43] = 0xff;
 
 #ifdef DEBUG
 	FILE* f = fopen("write_buffer", "w");
